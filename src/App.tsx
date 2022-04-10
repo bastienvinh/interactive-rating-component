02a610e700636be7aef503699626235d4ca99895
Bastien VINH <bvinh.dev@yahoo.fr>
2022-04-10 08:16:26 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const App: React.FC = () => {
+  return <div>
+    <h1>Super cool</h1>
+  </div>
+}
+
+export default App
---
+++ webpack.config.ts
@@ -0,0 +1,98 @@
+import path from "path"
+import { Configuration as DefaulConfiguration } from "webpack"
+import { Configuration as DevConfiguration } from "webpack-dev-server"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import sass from "sass"
+
+type Configuration = DefaulConfiguration & DevConfiguration
+
+const conf: Configuration = {
+  entry: "./src/bootstrap.tsx",
+  mode: "development",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+
+  cache: false,
+  experiments: {
+    cacheUnaffected: false,
+    topLevelAwait: true,
+  },
+
+  module: {
+    rules: [
+      // Rules for Typescript and TSX
+
+      // I use ts-loader because I don't want to transpile my code to ES5 Code, I like the new way
+      { test: /\.tsx?$/, loader: "ts-loader" },
+
+      { test: /\.(css|scss)$/, use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: 2,
+            modules: true
+          }
+        },
+        "postcss-loader",
+        {
+          loader: "sass-loader",
+          options: {
+            implementation: sass
+          }
+        }
+      ]},
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        // More information here https://webpack.js.org/guides/asset-modules/
+        type: "asset",
+      },
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js"
+  },
+  devServer: {
+    hot: true,
+    liveReload: true,
+    compress: true,
+    port: 7000,
+    historyApiFallback: true,
+    watchFiles: {
+      paths: ["src/**/*"]
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: {
+        removeComments: true
+      },
+      base: "/",
+      meta: {
+        "viewport": "width=device-width, initial-scale=1.0",
+      },
+      templateContent: ({ htmlWebpackPlugin }) => `
+        <html>
+          <head>
+            ${htmlWebpackPlugin.tags.headTags}
+          </head>
+          <body>
+            <noscript>You must have Javascript enabled to open our webpage</noscript>
+            <div id="root"></div>
+            ${htmlWebpackPlugin.tags.bodyTags}
+          </body>
+        </html>
+      `,
+      scriptLoading: "defer",
+      hash: true,
+      inject: "body",
+      title: "Portfolio",
+      filename: "index.html"
+    })
+  ]
+}
+
+
+export default conf
